8980022682fc42f5509aba195fdc8447e5021a67
Anthony Campbell <ancampbe@mtu.edu>
2013-12-11 14:44:39 -0500

    GUI -> Master

--- write.py
+++ write.py
@@ -8,49 +8,24 @@ from PIL import Image
 import cStringIO
 
 class EPDFWrite:
-  def __init__(self, filename, pages, tmp, fonts):
-    for font in fonts:
-      try:
-        ttf = TTFont(fonts[font], tmp + '/' + fonts[font] + '.ttf')
-        pdfmetrics.registerFont(ttf)
-      except: pass
-    size = pages[0].rect()
-    pdf = canvas.Canvas(filename, pagesize=(size.width(), size.height()))
+  def __init__(self, filename, pages, gfxview, gfx):
+    pageWidth = pages[0].rect().right()
+    pageHeight = pages[0].rect().bottom()
+    pdf = canvas.Canvas(filename, pagesize=(pageWidth, pageHeight))
     for page in pages:
-      size = page.rect()
-      top = size.y() + size.height()
-      for item in page.childItems(): self.drawItem(pdf, top, item)
+      pageImage = QtGui.QImage(pageWidth, pageHeight, QtGui.QImage.Format_RGB32)
+      gfx.render(QtGui.QPainter(pageImage), QtCore.QRectF(), QtCore.QRectF(0, page.rect().y(), pageWidth, pageHeight))
+      self.drawPage(pdf, pageImage)
       pdf.showPage()
     pdf.save()
 
-  def drawItem(self, pdf, pageheight, item):
-    if isinstance(item, QtGui.QGraphicsRectItem):
-      box = item.rect()
-      y = pageheight - box.y() - box.height()
-      stroke = item.pen().widthF()
-      fill = 0 if item.brush().style() == QtCore.Qt.NoBrush else 1
-      pdf.rect(box.x(), y, box.width(), box.height(), stroke, fill)
-    if isinstance(item, QtGui.QGraphicsLineItem):
-      line = item.line()
-      x1, x2 = line.x1(), line.x2()
-      y1, y2 = pageheight - line.y1(), pageheight - line.y2()
-      pdf.line(x1, y1, x2, y2)
-    #if isinstance(item, QtGui.QGraphicsPathItem):
-    if isinstance(item, QtGui.QGraphicsTextItem):
-      box = item.boundingRect()
-      txt = pdf.beginText()
-      txt.setTextOrigin(box.x(), pageheight - box.y())
-      parser = PDFHTMLParser(txt)
-      parser.feed(item.toHtml().toUtf8().data())
-      pdf.drawText(txt)
-    if isinstance(item, QtGui.QGraphicsPixmapItem):
-      box = item.boundingRect()
+  def drawPage(self, pdf, pageImage):
       buf = QtCore.QBuffer()
       buf.open(QtCore.QIODevice.ReadWrite)
-      item.pixmap().toImage().save(buf, 'PNG')
+      pageImage.save(buf, 'PNG')
       strio = cStringIO.StringIO()
       strio.write(buf.data())
       buf.close()
       strio.seek(0)
       img = Image.open(strio)
-      pdf.drawInlineImage(img, box.x(), pageheight - box.y() - box.height())
+      pdf.drawInlineImage(img, 0, 0)
